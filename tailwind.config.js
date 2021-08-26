c35d06c21f15cbc6938b93428168094b12aae3a6
Kaelem <kaelem.chandra@outlook.com>
2021-08-27 03:30:53 +1200

    change to media based darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      outline: {
+        solidwhite: '1px solid white',
+      }
+    },
   },
   variants: {
     extend: {},
