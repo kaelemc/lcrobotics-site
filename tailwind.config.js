5816aad4f88065ab2d641e623f1524418d2cd9c3
Kaelem <kaelem.chandra@outlook.com>
2021-08-28 02:04:21 +1200

    add bg image custom style to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: 'media', // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
+      zIndex: {
+        '-1': '-1',
+       },
+       backgroundImage: theme => ({
+        'hero': "url('./media/images/hero.jpg')",
+       }),
       outline: {
         solidwhite: '1px solid white',
-      }
+      },
     },
   },
   variants: {
