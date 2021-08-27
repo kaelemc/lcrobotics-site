04a5c68f6eb48fc7be10315565a5f7fb74ec71b5
Kaelem <kaelem.chandra@outlook.com>
2021-08-28 05:00:04 +1200

    finish sponsors

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import { LocationMarkerIcon } from "@heroicons/react/outline";
 import { Link } from "react-router-dom"
 import GasAppLogo from "../media/images/gas-appliance-sponsor.jpg"
 
-function Home() {
+const Home = () => {
     return (
         <div className="w-full">
           {/* Hero */}
@@ -25,7 +25,7 @@ function Home() {
                 </p>
                 <div className="mt-5">
                     <div className="rounded-md shadow lg:w-1/2">
-                      <a href="#sponsors" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-green-600 hover:bg-green-700">
+                      <a href="#about" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-green-600 hover:bg-green-700">
                         Learn More
                       </a>
                     </div>
@@ -36,20 +36,37 @@ function Home() {
               test
             </div>
           </div>
-          {/* sponsors Div */}
-          <div className="g">
-            
-          </div>
-          <div className="flex flex-col justify-center items-center px-10" id="sponsors">
+
+          {/* About Div */}
+          <div className="grid p-10" id="about">
             <div className="text-center">
-              <h1 className="text-4xl mt-8 mb-2">Our Sponsors!</h1>
-              <p className="text-gray-500 italic">We thank all our sponsors, we couldn't be able to do what we love without you guys!</p>
+              <h1 className="text-4xl mb-2">About Us</h1>
+              <p className="lg:mx-60">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin suscipit quam sed tincidunt consequat. Aenean vel porttitor lorem. 
+                Nullam id ultrices nisl. Aliquam ut augue sem. Duis in felis eget velit interdum viverra ut vitae mi. Cras sollicitudin, mi id egestas 
+                euismod, augue risus varius quam, nec dignissim magna sapien a leo. In quis mauris risus. Suspendisse quis magna accumsan, varius nunc 
+                interdum, posuere nunc. Integer ut ipsum nec nunc mattis varius..</p>
             </div>
-            <a href="https://gasappliancespecialists.nz/" target="new"><img src={GasAppLogo} alt="Gas Appliance Specialsts Logo"/></a>
-            <div className="my-8 w-full flex justify-center">
+            {/* More About Us Button */}
+            <div className="mt-8 w-full flex justify-center">
                 <div className="rounded-md shadow w-full lg:w-1/4">
                     <Link to="/about" className="flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-green-600 hover:bg-green-700">
-                    Learn More
+                    More About Us
+                    </Link>
+                </div>
+            </div>
+          </div>
+
+          {/* sponsors Div */}
+          <div className="flex flex-col justify-center items-center p-10 bg-gray-100" id="sponsors">
+            <div className="text-center mb-6">
+              <h1 className="text-4xl mb-2">Our Sponsors!</h1>
+              <p className="text-gray-500 italic">We thank all our sponsors, we couldn't be able to do what we love without you guys!<br/>(click on the logo to go to their websites)</p>
+            </div>
+            <a href="https://gasappliancespecialists.nz/" target="new"><img src={GasAppLogo} alt="Gas Appliance Specialsts Logo" className="h-auto rounded-md hover:shadow-md"/></a>
+            <div className="mt-8 w-full flex justify-center">
+                <div className="rounded-md shadow w-full lg:w-1/4">
+                    <Link to="/about" className="flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-green-600 hover:bg-green-700">
+                    More About Sponsorships
                     </Link>
                 </div>
             </div>
