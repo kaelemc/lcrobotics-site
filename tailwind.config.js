bd0c482126e7ea7faec4a05d664bcccf63f050cd
Kaelem <kaelem.chandra@outlook.com>
2021-11-21 05:03:12 +1300

    fix variant issue for display with groups

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,12 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: 'class', // or 'media' or 'class'
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
-    extend: {
-      zIndex: {
-        '-1': '-1',
-       },
-       colors: {
-         's-green' : '#05c42b',
-         's-gray' : '#111',
-         's-gray-2' : '#1a1a1a',
-         's-black' : '#000',
-      },
-       backgroundImage: theme => ({
-        'hero': "url('./media/images/hero.jpg')",
-       }),
-    },
-  },
-  variants: {
     extend: {},
   },
+  variants: {
+    extend: {
+      display: ["group-hover"],
+    },
+  },
   plugins: [],
-}
+};
