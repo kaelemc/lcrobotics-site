4ffd9bb25c3da1474c556c442c85b1a867af989e
Kaelem <kaelem.chandra@outlook.com>
2022-01-07 21:26:52 +1300

    make footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,5 @@
 const navigation = [
+    { name: "Home", href: "/", isMenu: false },
 	{
 		name: "About",
 		href: "/about",
@@ -27,45 +28,49 @@ const navigation = [
 export default function Footer() {
     return (
         <>
-        <div className="flex bg-black text-white px-4 py-10 justify-around items-center flex-row">
-            <div className="">
-                <h1 className="font-bold text-lg">
-                    Lynfield College Robotics<br/>
-                    <span className="text-right w-full block font-normal">2915</span>
-                </h1>
-                {/* socials */}
-                <div className="">
+        <div className="bg-gray-50">
+            <div className="max-w-screen-xl mx-auto">
+                <div className="flex justify-center md:justify-between items-center flex-row px-4 py-10">
+                    <div className="">
+                        <h1 className="font-semibold text-gray-900 text-lg">
+                            Lynfield College Robotics<br/>
+                            <span className="text-right w-full block font-normal">2915</span>
+                        </h1>
+                        {/* socials */}
+                        <div className="">
 
+                        </div>
+                    </div>
+                    {/* footer nav */}
+                    <ul className="hidden md:grid grid-flow-row md:grid-flow-col">
+                        {
+                            navigation.map((item) => {
+                                if(item.isMenu) {
+                                    return(
+                                        <ul className="px-4">
+                                            <li className="font-semibold text-gray-900 hover:text-blue-600 transition duration-300 ease-in-out"><a href={item.href}>{item.name}</a></li>
+                                            { item.childItems.map((childItem) => {
+                                                return(
+                                                    <li className="py-2"><a href={childItem.href} className="hover:text-blue-600 transition duration-300 ease-in-out">{childItem.name}</a></li>
+                                                );
+                                            }) 
+                                            }
+                                        </ul>
+                                    );
+                                }
+                                else {
+                                    return(
+                                        <li><a href={item.href} className="font-semibold text-gray-900 hover:text-blue-600 transition duration-300 ease-in-out">{item.name}</a></li>
+                                    );
+                                }
+                            })
+                        }
+                    </ul>
                 </div>
             </div>
-            {/* footer nav */}
-            <div className="hidden md:grid grid-flow-row md:grid-flow-col">
-                {
-                    navigation.map((item) => {
-                        if(item.isMenu) {
-                            return(
-                                <ul className="px-4">
-                                    <li className="text-gray-300 font-bold hover:text-gray-400 transition duration-300 ease-in-out"><a href={item.href}>{item.name}</a></li>
-                                    { item.childItems.map((childItem) => {
-                                        return(
-                                            <li className="py-2"><a href={childItem.href} className="hover:text-gray-300 transition duration-300 ease-in-out">{childItem.name}</a></li>
-                                        );
-                                    }) 
-                                    }
-                                </ul>
-                            );
-                        }
-                        else {
-                            return(
-                                <a href={item.href} className="text-gray-300 font-bold hover:text-gray-400 transition duration-300 ease-in-out inline-block">{item.name}</a>
-                            );
-                        }
-                    })
-                }
+            <div>
+                    <p className="text-center py-4">&copy; 2021-2022 Lynfield College Robotics. All rights reserved</p>
             </div>
-        </div>
-        <div className="bg-black text-white">
-            <p className="text-center pb-4">&copy; 2021 Lynfield College Robotics. All rights reserved</p>
         </div>
         </>
     );
