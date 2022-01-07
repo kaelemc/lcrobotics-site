cef705c68cb167005355de47c3de8f0afdc70750
Kaelem <kaelem.chandra@outlook.com>
2022-01-08 00:16:28 +1300

    create about page

---
+++ src/components/pages/about.js
@@ -0,0 +1,17 @@
+export default function About() {
+    return (
+       <>
+       <div className="max-w-screen-xl mx-auto">
+            {/* header */}
+            <div className="h-[35vh] flex justify-center items-center flex-col">
+                <h1 className="font-semibold text-5xl pt-16">About Us</h1>
+                <p className="text-xl">Tagline because taglines are nice</p>
+            </div>
+            {/* content section */}
+            <div className="">
+
+            </div>
+       </div>
+       </>
+    )
+}
