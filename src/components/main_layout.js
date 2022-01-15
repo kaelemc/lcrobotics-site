d6ee0cae4a5d3e5ca3a4eaa46c6ebbb6a94eb328
Kaelem <kaelem.chandra@outlook.com>
2022-01-16 02:12:58 +1300

    change body bg to be white and heading to gray-50

--- src/components/main_layout.js
+++ src/components/main_layout.js
@@ -2,12 +2,12 @@ export default function TemplateLayout(props) {
     return(
         <>
         {/* header */}
-        <div className="h-[35vh] flex justify-center items-center flex-col">
-            <h1 className="font-semibold text-5xl pt-16 text-gray-900">{props.title}</h1>
+        <div className="h-[35vh] flex justify-center items-center flex-col bg-gray-50">
+            <h1 className="font-semibold text-4xl pt-16 text-gray-900">{props.title}</h1>
             <p className="text-lg text-gray-700">{props.tagline}</p>
         </div>
         {/* content section */}
-        <main className="bg-gray-50">
+        <main className="">
             <div className="max-w-screen-xl mx-auto px-5 lg:px-0 py-32">
                 {props.children}
             </div>
